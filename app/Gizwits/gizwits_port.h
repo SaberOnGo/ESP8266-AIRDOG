96ce1d29c3d7ac3a61ff8535b43664c30722f532
SaberOnGo <dear_pi@126.com>
2019-02-18 13:19:31 +0800

    ESP8266-AIRDOG
    esp8266 project for airdog

---
+++ app/Gizwits/gizwits_port.h
@@ -0,0 +1,10 @@
+
+#ifndef  __GIZWITS_PORT_H__
+#define  __GIZWITS_PORT_H__
+
+
+#include "user_def.h"
+
+#endif
+
+
---
+++ app/driver/.output/eagle/debug/obj/gizwits_key.d
@@ -0,0 +1,36 @@
+.output/eagle/debug/obj/gizwits_key.o .output/eagle/debug/obj/gizwits_key.d : gizwits_key.c ../include/driver/gizwits_key.h \
+ ../include/user_def.h ../../include/ets_sys.h ../../include/c_types.h \
+ ../../include/eagle_soc.h ../../include/os_type.h \
+ ../../include/ets_sys.h ../../include/osapi.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/string.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/_ansi.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/newlib.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/config.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/ieeefp.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/features.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/xtensa/config/core-isa.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/reent.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/_ansi.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/_types.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/_types.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/_default_types.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/lock.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stddef.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/cdefs.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/string.h \
+ ../../include/os_type.h ../include/user_config.h ../../include/mem.h \
+ ../../include/user_interface.h ../../include/ip_addr.h \
+ ../../include/queue.h ../../include/spi_flash.h ../../include/gpio.h \
+ ../include/user_config.h ../include/driver/hal_key.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/stdio.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stdarg.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/types.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/types.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/stdio.h \
+ ../../include/c_types.h ../../include/gpio.h ../include/driver/key.h \
+ ../..//app/Gizwits/gizwits_product.h \
+ ../..//app/Gizwits/gizwits_protocol.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stdint.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/stdint.h \
+ ../include/user_webserver.h ../..//app/Utils/common.h \
+ ../../include/smartconfig.h ../../include/airkiss.h
